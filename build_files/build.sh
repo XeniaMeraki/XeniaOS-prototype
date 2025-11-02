72fc656861025aef1a0601e933d897732aee7777
Xenia Meraki <xeniameraki@protonmail.com>
2025-11-02 14:58:24 -0700

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -41,12 +41,34 @@ systemctl enable --global noctalia.service
 
 #replace Fedora kernel with CachyOS kernel
 rm -r -f /usr/lib/modules
-dnf -y copr enable bieszczaders/kernel-cachyos
-dnf -y install kernel-cachyos
+
+for pkg in kernel kernel-core kernel-modules kernel-modules-core; do
+  rpm --erase $pkg --nodeps
+done
+
+pushd /usr/lib/kernel/install.d
+printf '%s\n' '#!/bin/sh' 'exit 0' > 05-rpmostree.install
+printf '%s\n' '#!/bin/sh' 'exit 0' > 50-dracut.install
+chmod +x  05-rpmostree.install 50-dracut.install
+popd
+
+dnf -y copr enable bieszczaders/kernel-cachyos-lto
+dnf -y copr disable bieszczaders/kernel-cachyos-lto
+dnf -y --enablerepo copr:copr.fedorainfracloud.org:bieszczaders:kernel-cachyos-lto install \
+  kernel-cachyos-lto
+
 dnf -y copr enable bieszczaders/kernel-cachyos-addons
-dnf -y swap zram-generator-defaults cachyos-settings
-dnf -y install scx-scheds-git
-dnf -y install scx-manager
+dnf -y copr disable bieszczaders/kernel-cachyos-addons
+dnf -y --enablerepo copr:copr.fedorainfracloud.org:bieszczaders:kernel-cachyos-addons swap zram-generator-defaults cachyos-settings
+dnf -y --enablerepo copr:copr.fedorainfracloud.org:bieszczaders:kernel-cachyos-addons install \
+  scx-scheds-git \
+  cachyos-ksm-settings \
+  scx-manager
+
+# TODO: remove once https://github.com/CachyOS/copr-linux-cachyos/pull/69 is merged
+install -Dpm0644 -t /usr/lib/systemd/system/greetd.service.d /usr/lib/systemd/system/user@.service.d/10-ksm.conf
+
+# Probably then normal kernel initialization idk
 
 KERNEL_VERSION="$(find "/usr/lib/modules" -maxdepth 1 -type d ! -path "/usr/lib/modules" -exec basename '{}' ';' | sort | tail -n 1)"
 export DRACUT_NO_XATTR=1
